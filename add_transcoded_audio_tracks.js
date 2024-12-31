8e77471c66827aa4bc43f02c4b2c2f6437de28f5
Damien Dessagne <damien@embers.games>
2024-12-31 13:32:57 +0100

    Fixed inconsistent indents

--- add_transcoded_audio_tracks.js
+++ add_transcoded_audio_tracks.js
@@ -15,7 +15,7 @@ const details = () => ({
     Version: '1.0',
     Tags: 'action,audio',
     Inputs: [
-		{
+        {
             name: 'targetCodec',
             type: 'string',
             label: 'Target Codec',
@@ -30,7 +30,7 @@ const details = () => ({
                     'opus',
                     'flac',
                     'vorbis',
-					'truehd',
+                    'truehd',
                 ],
             },
             tooltip: 'Select the codec to which the audio tracks should be converted.',
@@ -49,19 +49,19 @@ const details = () => ({
 const plugin = (file, libraryOptions, inputs) => {
     const response = {
         processFile: false, // Set to true if the file needs processing
-		container: `.${file.container}`,
-		handBrakeMode: false,
-		FFmpegMode: true,
-		infoLog: '',
+        container: `.${file.container}`,
+        handBrakeMode: false,
+        FFmpegMode: true,
+        infoLog: '',
     };
 
-	// Log utility function
-	function log(entry) {
-		console.log(entry);
-		response.infoLog += entry + "\n";
-	}
-	
-	const pluginWatermark = "[Tdarr:add_transcoded_audio_tracks:processed]";
+    // Log utility function
+    function log(entry) {
+        console.log(entry);
+        response.infoLog += entry + "\n";
+    }
+    
+    const pluginWatermark = "[Tdarr:add_transcoded_audio_tracks:processed]";
 
     // Retrieve the list of codecs to convert and the target codec from the inputs
     const codecsToConvert = inputs.codecsToConvert ? inputs.codecsToConvert.split(',').map(c => c.trim().toLowerCase()) : ['truehd', 'eac3', 'dts'];
@@ -74,51 +74,51 @@ const plugin = (file, libraryOptions, inputs) => {
     };
 
     // Check if the file is valid for processing
-	log("Checking for ffProbeData...");
+    log("Checking for ffProbeData...");
     if (!file.ffProbeData || !file.ffProbeData.streams) {
         log('Invalid file for processing, FFProbe data missing');
         return response;
     }
-	
-	log("FFProbeData present, extracting audio tracks data...");
+    
+    log("FFProbeData present, extracting audio tracks data...");
     const audioTracks = file.ffProbeData.streams.filter(stream => stream.codec_type === 'audio');
     if (audioTracks.length === 0) {
         log('No audio tracks, nothing to do');
         return response;
     }
-	log(audioTracks.length + " audio tracks");
-	
-	// Check plugin didn't process the file already
-	let copyrightData = file.ffProbeData.format && file.ffProbeData.format.tags && file.ffProbeData.format.tags.COPYRIGHT || '';
-	if (copyrightData.includes(pluginWatermark)) {
-		log('Plugin watermark found -> file already processed. Aborting.');
-		return response;
-	}
+    log(audioTracks.length + " audio tracks");
+    
+    // Check plugin didn't process the file already
+    let copyrightData = file.ffProbeData.format && file.ffProbeData.format.tags && file.ffProbeData.format.tags.COPYRIGHT || '';
+    if (copyrightData.includes(pluginWatermark)) {
+        log('Plugin watermark found -> file already processed. Aborting.');
+        return response;
+    }
 
     // Check each audio track
     const newAudioTracks = [];
-	let audioTrackIndex = -1;
-	let newAudioTrackIndex = -1;
+    let audioTrackIndex = -1;
+    let newAudioTrackIndex = -1;
     audioTracks.forEach((track) => {
-		audioTrackIndex += 1;
-		newAudioTrackIndex += 1;
-		
-		newAudioTracks.push(`-map 0:a:${audioTrackIndex} -c:a:${newAudioTrackIndex} copy`); // Copy the original audio track
-		
+        audioTrackIndex += 1;
+        newAudioTrackIndex += 1;
+        
+        newAudioTracks.push(`-map 0:a:${audioTrackIndex} -c:a:${newAudioTrackIndex} copy`); // Copy the original audio track
+        
         // Skip if the track is already in the target codec
         if (track.codec_name.toLowerCase() === targetCodec) {
-			log("Track " + audioTrackIndex + " is already in a target codec (" + track.codec_name + ").");
+            log("Track " + audioTrackIndex + " is already in a target codec (" + track.codec_name + ").");
             return; // No need to transcode this track
         }
 
         if (codecsToConvert.includes(track.codec_name.toLowerCase())) {
-			newAudioTrackIndex += 1; // -> Put the transcoded track right after the original one
-			
-			log("Track " + audioTrackIndex + " is in a codec to convert (" + track.codec_name + "), adding the transcoded track right after...");
+            newAudioTrackIndex += 1; // -> Put the transcoded track right after the original one
+            
+            log("Track " + audioTrackIndex + " is in a codec to convert (" + track.codec_name + "), adding the transcoded track right after...");
             let bitrate = track.bit_rate ? parseInt(track.bit_rate, 10) : 128000; // Default to 128 kbps if bitrate is undefined
             let channels = track.channels || 2; // Default to stereo if channels are undefined
             const lang = track.tags && track.tags.language ? track.tags.language : 'und';
-			const title = (track.tags && track.tags.title ? track.tags.title + ' -> ' : lang.toUpperCase()) + ` ${targetCodec.toUpperCase()} ${channels}ch ${bitrate/1000}kbps [Auto]`;
+            const title = (track.tags && track.tags.title ? track.tags.title + ' -> ' : lang.toUpperCase()) + ` ${targetCodec.toUpperCase()} ${channels}ch ${bitrate/1000}kbps [Auto]`;
 
             // Adjust channels based on codec limitations
             if (targetCodec === 'ac3' && channels > 6) {
@@ -138,24 +138,24 @@ const plugin = (file, libraryOptions, inputs) => {
             newAudioTracks.push(`-b:a:${newAudioTrackIndex} ${bitrate}`); // Set bitrate
             newAudioTracks.push(`-ac:${newAudioTrackIndex} ${channels}`); // Set channels
             newAudioTracks.push(`-metadata:s:a:${newAudioTrackIndex} language=${lang}`); // Set language
-			newAudioTracks.push(`-metadata:s:a:${newAudioTrackIndex} 'title=${title}'`); // Set track title
+            newAudioTracks.push(`-metadata:s:a:${newAudioTrackIndex} 'title=${title}'`); // Set track title
         }
-		else {
-			log("Track " + audioTrackIndex + " doesn't require transcoding (" + track.codec_name + ").");
-		}
+        else {
+            log("Track " + audioTrackIndex + " doesn't require transcoding (" + track.codec_name + ").");
+        }
     });
 
     // If new audio tracks were created, build the ffmpeg command
     if (newAudioTracks.length > 0) {
         response.processFile = true;
         response.preset = ',' + 
-			'-map 0:v -c:v copy ' + // Copy video stream without re-encoding
-			newAudioTracks.join(' ') + ' ' + // Add audio tracks
+            '-map 0:v -c:v copy ' + // Copy video stream without re-encoding
+            newAudioTracks.join(' ') + ' ' + // Add audio tracks
             '-map 0:s? ' + // Copy subtitles
-			'-metadata \'copyright=' + copyrightData + pluginWatermark + '\''; // Add plugin watermark
+            '-metadata \'copyright=' + copyrightData + pluginWatermark + '\''; // Add plugin watermark
         log(`Added ${targetCodec} tracks to audio streams`);
     } else {
-		log("Nothing to convert.");
+        log("Nothing to convert.");
     }
 
     return response;
