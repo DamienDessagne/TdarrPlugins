cb3986d392302f365e8b15b9085be95d17337cb1
Damien Dessagne <damien@embers.games>
2025-01-01 17:03:31 +0100

    Added more parameters and control to the plugin (overwrite original track, max bitrate, channels, etc.) Added ability to chain the plugin with different configurations in Tdarr Switched default parameters to AC3 instead of AAC Improved logging

--- add_transcoded_audio_tracks.js
+++ add_transcoded_audio_tracks.js
@@ -6,162 +6,230 @@
 
 // Plugin details (metadata)
 const details = () => ({
-    id: 'add_transcoded_audio_tracks',
-    Stage: 'Pre-processing',
-    Name: 'Add Transcoded Audio Tracks',
-    Type: 'Audio',
-    Operation: 'Transcode',
-    Description: 'Adds transcoded tracks for specified audio codecs while retaining original streams.',
-    Version: '1.0',
-    Tags: 'action,audio',
-    Inputs: [
-        {
-            name: 'targetCodec',
-            type: 'string',
-            label: 'Target Codec',
-            defaultValue: 'aac',
-            inputUI: {
-                type: 'dropdown',
-                options: [
-                    'aac',
-                    'ac3',
-                    'eac3',
-                    'mp3',
-                    'opus',
-                    'flac',
-                    'vorbis',
-                    'truehd',
-                ],
-            },
-            tooltip: 'Select the codec to which the audio tracks should be converted.',
-        },
-        {
-            name: 'codecsToConvert',
-            type: 'string',
-            label: 'Codecs to Convert',
-            defaultValue: 'truehd,eac3,dts',
-            tooltip: 'Comma-separated list of codecs to convert.',
-        },
-    ],
+	id: 'add_transcoded_audio_tracks',
+	Stage: 'Pre-processing',
+	Name: 'Add Transcoded Audio Tracks',
+	Type: 'Audio',
+	Operation: 'Transcode',
+	Description: 'Adds transcoded tracks for specified audio codecs, retaining or replacing original streams.',
+	Version: '1.0',
+	Tags: 'action,audio',
+	Inputs: [
+		{
+			name: 'codecsToConvert',
+			type: 'string',
+			defaultValue: 'truehd,eac3,dts',
+			tooltip: 'Comma-separated list of codecs to convert.',
+		},
+		{
+			name: 'targetCodec',
+			type: 'string',
+			defaultValue: 'ac3',
+			inputUI: {
+				type: 'dropdown',
+				options: [
+					'aac',
+					'ac3',
+					'eac3',
+					'flac',
+					'libmp3lame',
+					'libopus',
+					'truehd',
+					'libvorbis',
+					'custom',
+				],
+			},
+			tooltip: 'Select the target codec to which the audio tracks should be converted. If "custom" is selected, customTargetCodec\'s value will be used.',
+		},
+		{
+			name: 'customTargetCodec',
+			type: 'string',
+			defaultValue: '',
+			tooltip: 'When "custom" is selected as a target codec, this is the codec to which the audio tracks will be converted to. You can use the command ffmpeg -encoders for a list of encoders',
+		},
+		{
+			name: 'maxChannels',
+			type: 'int',
+			defaultValue: '-1',
+			inputUI: {
+				type: 'dropdown',
+				options: [
+					'-1',
+					'1',
+					'2',
+					'3',
+					'6',
+					'8',
+				],
+			},
+			tooltip: 'Maximum number of channels. -1 = same as original track. AC3 is already limited internally to 6 channels, and MP3 to 2.',
+		},
+		{
+			name: 'maxBitrate',
+			type: 'int',
+			defaultValue: '-1',
+			tooltip: 'Maximum bitrate in bps. -1 = keep the original track bitrate. AC3 is already limited internally to 640kbps, and MP3 to 320kbps.',
+		},
+		{
+			name: 'losslessDefaultBitrate',
+			type: 'int',
+			defaultValue: '640000',
+			tooltip: 'Bitrate to default to when original audio track is in a lossless format, in bps.',
+		},
+		{
+			name: 'overwriteTrack',
+			type: 'boolean',
+			defaultValue: 'false',
+			inputUI: {
+				type: 'dropdown',
+				options: [
+					'true',
+					'false',
+				],
+			},
+			tooltip: 'Whether to overwrite the original audio track with the transcoded track (true), or add the transcoded audio track after the original audio track (false)',
+		},
+	],
 });
 
 // Plugin logic (functionality)
 const plugin = (file, libraryOptions, inputs) => {
-    const response = {
-        processFile: false, // Set to true if the file needs processing
-        container: `.${file.container}`,
-        handBrakeMode: false,
-        FFmpegMode: true,
-        infoLog: '',
-    };
+	const response = {
+		processFile: false, // Set to true if the file needs processing
+		container: `.${file.container}`,
+		handBrakeMode: false,
+		FFmpegMode: true,
+		infoLog: '',
+	};
 
-    // Log utility function
-    function log(entry) {
-        console.log(entry);
-        response.infoLog += entry + "\n";
-    }
-    
-    const pluginWatermark = "[Tdarr:add_transcoded_audio_tracks:processed]";
+	// Log utility function
+	function log(entry) {
+		console.log(entry);
+		response.infoLog += entry + "\n";
+	}
 	
-	log("--- Starting Add Transcoded Audio Tracks plugin on " + file.file + " ...");
+	log(`--- Starting Add Transcoded Audio Tracks plugin on ${file.file} ...`);
 
-    // Retrieve the list of codecs to convert and the target codec from the inputs
-    const codecsToConvert = inputs.codecsToConvert ? inputs.codecsToConvert.split(',').map(c => c.trim().toLowerCase()) : ['truehd', 'eac3', 'dts'];
-    const targetCodec = inputs.targetCodec || 'aac';
+	// Retrieve the list of codecs to convert and the target codec from the inputs
+	const codecsToConvert = inputs.codecsToConvert.split(',').map(c => c.trim().toLowerCase());
+	const targetCodec = inputs.targetCodec === 'custom' ? inputs.customTargetCodec : inputs.targetCodec;
+	const maxChannels = inputs.maxChannels;
+	const maxBitrate = inputs.maxBitrate;
+	const losslessDefaultBitrate = inputs.losslessDefaultBitrate;
+	const overwriteTrack = inputs.overwriteTrack === 'true';
+	
+	const pluginWatermark = `[Tdarr:add_transcoded_audio_tracks:${details().Version}:${codecsToConvert.join('|')}:${targetCodec}]`;
+	
+	// Define limitations for specific codecs
+	const codecChannelsLimits = {
+		ac3: 6, // AC3 maximum channels is 6
+		mp3: 2, // AC3 maximum channels is 6
+	};
+	const codecBitrateLimits = {
+		ac3: 640000, // AC3 maximum bitrate is 640 kbps
+		mp3: 320000, // MP3 maximum bitrate is 320 kbps
+	};
 
-    // Define bitrate limitations for specific codecs
-    const codecBitrateLimits = {
-        ac3: 640000, // AC3 maximum bitrate is 640 kbps
-        mp3: 320000, // MP3 maximum bitrate is 320 kbps
-    };
+	// Check if the file is valid for processing
+	log("Checking for ffProbeData...");
+	if (!file.ffProbeData || !file.ffProbeData.streams) {
+		log('Invalid file for processing, FFProbe data missing');
+		return response;
+	}
 
-    // Check if the file is valid for processing
-    log("Checking for ffProbeData...");
-    if (!file.ffProbeData || !file.ffProbeData.streams) {
-        log('Invalid file for processing, FFProbe data missing');
-        return response;
-    }
+	// Check plugin didn't process the file already
+	let copyrightData = file.ffProbeData.format && file.ffProbeData.format.tags && file.ffProbeData.format.tags.COPYRIGHT || '';
+	if (copyrightData.includes(pluginWatermark)) {
+		log('Plugin watermark found -> file already processed. Aborting.');
+		return response;
+	}
 
-    // Check plugin didn't process the file already
-    let copyrightData = file.ffProbeData.format && file.ffProbeData.format.tags && file.ffProbeData.format.tags.COPYRIGHT || '';
-    if (copyrightData.includes(pluginWatermark)) {
-        log('Plugin watermark found -> file already processed. Aborting.');
-        return response;
-    }
-
-    // Extract audio tracks data
+	// Extract audio tracks data
 	log("FFProbeData present, extracting audio tracks data...");
-    const audioTracks = file.ffProbeData.streams.filter(stream => stream.codec_type === 'audio');
-    if (audioTracks.length === 0) {
-        log('No audio tracks, nothing to do');
-        return response;
-    }
-    log(audioTracks.length + " audio tracks");
-    
-    // Check each audio track
-    const newAudioTracks = [];
-    let audioTrackIndex = -1;
-    let newAudioTrackIndex = -1;
-    audioTracks.forEach((track) => {
-        audioTrackIndex += 1;
-        newAudioTrackIndex += 1;
-        
-        newAudioTracks.push(`-map 0:a:${audioTrackIndex} -c:a:${newAudioTrackIndex} copy`); // Copy the original audio track
-        
-        // Skip if the track is already in the target codec
-        if (track.codec_name.toLowerCase() === targetCodec) {
-            log("Track " + audioTrackIndex + " is already in a target codec (" + track.codec_name + ").");
-            return; // No need to transcode this track
-        }
+	const audioTracks = file.ffProbeData.streams.filter(stream => stream.codec_type === 'audio');
+	if (audioTracks.length === 0) {
+		log('No audio tracks, nothing to do');
+		return response;
+	}
+	log(audioTracks.length + " audio tracks");
+	
+	// Check each audio track
+	const audioTracksCommands = [];
+	let inputAudioTrackIndex = -1;
+	let outputAudioTrackIndex = -1;
+	let requireTranscode = false;
+	audioTracks.forEach((track) => {
+		inputAudioTrackIndex += 1;
+		outputAudioTrackIndex += 1;
+		
+		// Transcode track if needed
+		if (codecsToConvert.includes(track.codec_name.toLowerCase())) {
+			log(`Track ${inputAudioTrackIndex} is in a codec to convert (${track.codec_name.toUpperCase()})`);
+			requireTranscode = true;
+			
+			// Prepare transcoded track parameters
+			let bitrate = track.bit_rate ? parseInt(track.bit_rate) : losslessDefaultBitrate; // Default to losslessDefaultBitrate if bitrate is undefined
+			let channels = track.channels || 2; // Default to stereo if channels are undefined
+			const lang = track.tags && track.tags.language ? track.tags.language : 'und';
 
-        if (codecsToConvert.includes(track.codec_name.toLowerCase())) {
-            newAudioTrackIndex += 1; // -> Put the transcoded track right after the original one
-            
-            log("Track " + audioTrackIndex + " is in a codec to convert (" + track.codec_name + "), adding the transcoded track right after...");
-            let bitrate = track.bit_rate ? parseInt(track.bit_rate, 10) : 128000; // Default to 128 kbps if bitrate is undefined
-            let channels = track.channels || 2; // Default to stereo if channels are undefined
-            const lang = track.tags && track.tags.language ? track.tags.language : 'und';
-            const title = (track.tags && track.tags.title ? track.tags.title + ' -> ' : lang.toUpperCase()) + ` ${targetCodec.toUpperCase()} ${channels}ch ${bitrate/1000}kbps [Auto]`;
+			// Adjust based on codec limitations
+			if(codecChannelsLimits[targetCodec] && channels > codecChannelsLimits[targetCodec]) {
+				log(`Downmixing to ${codecChannelsLimits[targetCodec]}ch from ${channels}ch since ${targetCodec} limits channels to ${codecChannelsLimits[targetCodec]}`);
+				channels = codecChannelsLimits[targetCodec];
+			}
+			if (codecBitrateLimits[targetCodec] && bitrate > codecBitrateLimits[targetCodec]) {
+				log(`Limiting bitrate to ${codecBitrateLimits[targetCodec]}bps from ${bitrate}bps since ${targetCodec} limits bitrate to ${codecBitrateLimits[targetCodec]}`);
+				bitrate = codecBitrateLimits[targetCodec];
+			}
 
-            // Adjust channels based on codec limitations
-            if (targetCodec === 'ac3' && channels > 6) {
-                channels = 6; // AC3 supports a maximum of 6 channels
-            } else if (targetCodec === 'mp3' && channels > 2) {
-                channels = 2; // MP3 supports a maximum of 2 channels
-            }
+			// Adjust based on user-specified limitations
+			if(maxChannels > 0 && channels > maxChannels) {
+				log(`Limiting channels to ${maxChannels}ch as stipulated by the user.`);
+				channels = maxChannels;
+			}
+			if(maxBitrate > 0 && bitrate > maxBitrate) {
+				log(`Limiting bitrate to ${maxBitrate}bps as stipulated by the user.`);
+				bitrate = maxBitrate;
+			}
+			
+			// Prepare track title
+			const title = (track.tags && track.tags.title ? track.tags.title + ' -> ' : lang.toUpperCase()) + ` ${targetCodec.toUpperCase()} ${channels}ch ${bitrate/1000}kbps [Auto]`;
+			
+			// Copy original track if no overwrite required
+			if(!overwriteTrack) {
+				log('Copying original audio track');
+				audioTracksCommands.push(`-map 0:a:${inputAudioTrackIndex} -c:a:${outputAudioTrackIndex} copy`); // Copy the original audio track
+				outputAudioTrackIndex += 1;
+			}
+			
+			// Create ffmpeg command to add the target codec track
+			log('Adding transcoded audio track');
+			audioTracksCommands.push(`-map 0:a:${inputAudioTrackIndex}`); // Map the original audio track
+			audioTracksCommands.push(`-c:a:${outputAudioTrackIndex} ${targetCodec}`); // Set the new track's target codec
+			audioTracksCommands.push(`-b:a:${outputAudioTrackIndex} ${bitrate}`); // Set bitrate
+			audioTracksCommands.push(`-ac:${outputAudioTrackIndex} ${channels}`); // Set channels
+			audioTracksCommands.push(`-metadata:s:a:${outputAudioTrackIndex} 'language=${lang}'`); // Set language
+			audioTracksCommands.push(`-metadata:s:a:${outputAudioTrackIndex} 'title=${title}'`); // Set track title
+		}
+		else { // -> copy the original track
+			log(`Track ${inputAudioTrackIndex} doesn't require transcoding (${track.codec_name.toUpperCase()}), copying the original track`);
+			audioTracksCommands.push(`-map 0:a:${inputAudioTrackIndex} -c:a:${outputAudioTrackIndex} copy`); // Copy the original audio track
+		}
+	});
 
-            // Adjust bitrate based on codec limitations
-            if (codecBitrateLimits[targetCodec] && bitrate > codecBitrateLimits[targetCodec]) {
-                bitrate = codecBitrateLimits[targetCodec];
-            }
+	// If new audio tracks were created, build the ffmpeg command
+	if (requireTranscode) {
+		response.processFile = true;
+		response.preset = ',' + 
+			'-map 0:v -c:v copy ' + // Copy video stream without re-encoding
+			audioTracksCommands.join(' ') + ' ' + // Add audio tracks
+			'-map 0:s? -c:s copy ' + // Copy subtitles
+			'-metadata \'copyright=' + copyrightData + pluginWatermark + '\''; // Add plugin watermark
+	} else {
+		log("Nothing to convert.");
+	}
 
-            // Create ffmpeg command to add the target codec track
-            newAudioTracks.push(`-map 0:a:${audioTrackIndex}`); // Map the original audio track
-            newAudioTracks.push(`-c:a:${newAudioTrackIndex} ${targetCodec}`); // Set the new track's target codec
-            newAudioTracks.push(`-b:a:${newAudioTrackIndex} ${bitrate}`); // Set bitrate
-            newAudioTracks.push(`-ac:${newAudioTrackIndex} ${channels}`); // Set channels
-            newAudioTracks.push(`-metadata:s:a:${newAudioTrackIndex} language=${lang}`); // Set language
-            newAudioTracks.push(`-metadata:s:a:${newAudioTrackIndex} 'title=${title}'`); // Set track title
-        }
-        else {
-            log("Track " + audioTrackIndex + " doesn't require transcoding (" + track.codec_name + ").");
-        }
-    });
-
-    // If new audio tracks were created, build the ffmpeg command
-    if (newAudioTrackIndex > audioTrackIndex) {
-        response.processFile = true;
-        response.preset = ',' + 
-            '-map 0:v -c:v copy ' + // Copy video stream without re-encoding
-            newAudioTracks.join(' ') + ' ' + // Add audio tracks
-            '-map 0:s? -c:s copy ' + // Copy subtitles
-            '-metadata \'copyright=' + copyrightData + pluginWatermark + '\''; // Add plugin watermark
-        log(`Added ${newAudioTrackIndex-audioTrackIndex} transcoded ${targetCodec.toUpperCase()} tracks to audio streams`);
-    } else {
-        log("Nothing to convert.");
-    }
-
-    return response;
+	return response;
 };
 
 // Export the plugin (details and functionality)
